c3da873882c1bf80af336511389dab456763a972
Federica Blanco <federicablanco@MacBook-Pro-de-Federica.local>
2022-02-10 12:35:39 -0300

    Start

---
+++ vendor/src/showCommand.php
@@ -0,0 +1,11 @@
+<?php namespace Acme;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+
+class showCommand extends Command{
+
+}
