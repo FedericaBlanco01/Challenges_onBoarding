5207e9983ad75bede03fc045d2f867e250c0d4af
Federica Blanco <federicablanco@MacBook-Pro-de-Federica.local>
2022-02-11 15:22:03 -0300

    finished challenge 3

--- CHALLENGE_3/src/showCommand.php
+++ CHALLENGE_3/src/showCommand.php
@@ -8,10 +8,11 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Acme\RenderCommands;
+use Symfony\Component\Console\Helper\Table;
 
 use GuzzleHttp\Client;
 
-class showCommand extends Command
+class ShowCommand extends Command
 {
 
     public function configure()
@@ -22,36 +23,46 @@ class showCommand extends Command
             ->addOption('fullPlot', null, InputOption::VALUE_NONE, 'presents the plot of the movie');
     }
 
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute(InputInterface $input, OutputInterface $output) :int
     {
+        $optionValue = $input->getOption('fullPlot');
+        $nameMovie= $input->getArgument('title');
+        $arr_body= $this->request($nameMovie, $optionValue);
+        $this-> renderTable($output, $arr_body);
+        return 0;
+    }
 
+    public function renderTable($output, $arr_body){
+        $table = new Table($output);
+        $table->setHeaders([$arr_body['Title']. " - ".$arr_body['Year']]);
+        $rows= [];
+
+        foreach($arr_body as $key => $value) {
+            if (is_scalar($value) || is_null($value)) {
+                $rows[] = [$key, $value];
+            }
+        }
+        $table->setRows($rows);
+        $table->render();
+    }
+
+    public function request($name, $option)
+    {
         $client = new Client([
             // Base URI is used with relative requests
             'base_uri' => 'http://www.omdbapi.com/'
         ]);
 
-        $optionValue = $input->getOption('fullPlot');
-        $optional = '';
-
-        if ($optionValue === false) {
-            $optional = 'short';
-        } else {
-            $optional = 'full';
-        }
-
         $response = $client->request('GET', '/', [
             'query' => [
-                't' => $input->getArgument('title'),
-                'plot' => $optional,
+                't' => $name,
+                'plot' => $option ? 'full':'short',
                 'apikey' => 'f3c6067d'
             ]
         ]);
 
-
         $body = $response->getBody();
-        $arr_body = json_decode($body); //code from json to php object
-
-        print ($arr_body);
-
+        return json_decode($body, true);
+        
     }
 }
