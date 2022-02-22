32baf8a181366784a08feb4cff59e1c3febb76f7
Federica Blanco <federicablanco@MacBook-Pro-de-Federica.local>
2022-02-22 11:38:26 -0300

    flight,airline,city,available migrations

---
+++ Challenge_4/flights/database/migrations/2022_02_22_135954_create_flights_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id();
+            $table-> foreignId('airline_id');
+            $table-> foreignId('departure_city');
+            $table-> foreignId('arrival_city');
+            $table->dateTime('departure_time');
+            $table->timestamps('arrival_time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flights');
+    }
+};
---
+++ Challenge_4/flights/database/migrations/2022_02_22_143352_create_available_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('available', function (Blueprint $table) {
+            $table->id();
+            $table-> foreignId('airline_id');
+            $table-> foreignId('city_id');
+            $table->unique(['airline_id', 'city_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('available');
+    }
+};
