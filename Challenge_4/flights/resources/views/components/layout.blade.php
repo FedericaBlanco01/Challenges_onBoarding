ee1e601b68ccf76a86c3a659206fb01262522d8c
Federica <federica@lightit.io>
2022-03-15 16:30:38 -0300

    vue

--- Challenge_4/flights/resources/views/components/layout.blade.php
+++ Challenge_4/flights/resources/views/components/layout.blade.php
@@ -66,6 +66,7 @@
                             Subscribe
                         </button>
 
+                        <script src="{{asset('/js/app.js')}}"></script>
                         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
                         @yield('scripts')
 
