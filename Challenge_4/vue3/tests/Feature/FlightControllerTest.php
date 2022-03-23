d89d78f84fd6871f1b161438a4b75ca5fad6d1a7
Federica <federica@lightit.io>
2022-03-23 16:51:27 -0300

    basic flight testing

--- Challenge_4/vue3/tests/Feature/FlightControllerTest.php
+++ Challenge_4/vue3/tests/Feature/FlightControllerTest.php
@@ -2,10 +2,9 @@
 
 namespace Tests\Feature;
 
-use App\Http\Controllers\FlightController;
 use App\Models\Airline;
-use App\Models\Flight;
 use App\Models\City;
+use App\Models\Flight;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
@@ -18,7 +17,7 @@ class FlightControllerTest extends TestCase
     {
         $this->withoutExceptionHandling();
         $flights = Flight::factory()->create();
-        $route = '/deleteFlight/' . $flights->id;
+        $route = '/deleteFlight/'.$flights->id;
         $response = $this->call('DELETE', $route);
         $this->assertEquals(200, $response->status());
     }
@@ -35,7 +34,7 @@ class FlightControllerTest extends TestCase
         )->create();
         $flight = Flight::factory()->create([
             'id' => 1,
-            "airline_id" => 1,]);
+            'airline_id' => 1, ]);
 
         $route = '/updateflight';
         $response = $this->call('PATCH', $route, [
@@ -44,15 +43,15 @@ class FlightControllerTest extends TestCase
             'departure_city_id' => 1,
             'arrival_city_id' => 2,
             'departure_time' => '2022-12-12 07:16:11.0',
-            'arrival_time' => '2022-12-13 00:10:17.0']);
+            'arrival_time' => '2022-12-13 00:10:17.0', ]);
 
-        $this->assertDatabaseMissing('flights',[
+        $this->assertDatabaseMissing('flights', [
             'airline_id' => 1,
             ]);
-
     }
 
-    public function test_add_new_flight_to_db(){
+    public function test_add_new_flight_to_db()
+    {
         City::factory()->count(2)->sequence(
             ['id'=>1],
             ['id'=>2],
@@ -62,33 +61,30 @@ class FlightControllerTest extends TestCase
         )->create();
 
         $route = '/addflight';
-        $response = $this->call('POST', $route,[
+        $response = $this->call('POST', $route, [
             'airline_id' => 1,
             'departure_city_id' => 1,
             'arrival_city_id' => 2,
             'departure_time' => '2022-12-12 07:16:11.0',
-            'arrival_time' => '2022-12-13 00:10:17.0']);
+            'arrival_time' => '2022-12-13 00:10:17.0', ]);
 
-        $this->assertDatabaseHas('flights',[
+        $this->assertDatabaseHas('flights', [
             'airline_id' => 1,
             'departure_city_id' => 1,
             'arrival_city_id' => 2,
             'departure_time' => '2022-12-12 07:16:11.0',
-            'arrival_time' => '2022-12-13 00:10:17.0']);
+            'arrival_time' => '2022-12-13 00:10:17.0', ]);
     }
 
-    public function test_table_flights_displayed(){
+    public function test_table_flights_displayed()
+    {
         $this->withoutExceptionHandling();
         Flight::factory()->count(10)->create();
         $route = '/getflights';
         $this->call('GET', $route)
             ->assertSuccessful()
-            ->assertJson(function(AssertableJson $json){
-                $json->has('flights',10);
+            ->assertJson(function (AssertableJson $json) {
+                $json->has('flights', 10);
             });
-
     }
-    
-
-
 }
